928bd951e37be8a153b74e40119132f572bf0485
Alecsandro Da Silva <141873234+alex247417@users.noreply.github.com>
2025-04-15 17:40:03 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+using MinhaApi.Data;
+using MinhaApi.Routes;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApi();
+builder.Services.AddScoped<PersonContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+app.PersonRoutes();
+app.UseHttpsRedirection();
+app.Run();
+
+
